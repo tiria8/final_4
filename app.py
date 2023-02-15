0138e7da76762e4fc65a435505d49a380945da8d
daren <titadarenka@gmail.com>
2023-02-15 14:03:44 +0300

    edited

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask_restx import Api
 
 from config import Config
 from setup_db import db
+from views.auth import auth_ns
 from views.directors import director_ns
 from views.genres import genre_ns
 from views.movies import movie_ns
+from views.users import user_ns
 
 
 def create_app(config_object):
@@ -21,7 +23,8 @@ def register_extensions(app):
     api.add_namespace(director_ns)
     api.add_namespace(genre_ns)
     api.add_namespace(movie_ns)
-
+    api.add_namespace(user_ns)
+    api.add_namespace(auth_ns)
 
 app = create_app(Config())
 app.debug = True
--- views/genres.py
+++ views/genres.py
@@ -1,22 +1,44 @@
+from flask import request
 from flask_restx import Resource, Namespace
 
 from dao.model.genre import GenreSchema
 from implemented import genre_service
+from decorators import auth_required, admin_required
 
 genre_ns = Namespace('genres')
 
 
 @genre_ns.route('/')
 class GenresView(Resource):
+    @auth_required
     def get(self):
         rs = genre_service.get_all()
         res = GenreSchema(many=True).dump(rs)
         return res, 200
 
+    @admin_required
+    def post(self):
+        req_json = request.json
+        genre = genre_service.create(req_json)
+        return "", 201, {"location": f"/genres/{genre.id}"}
 
 @genre_ns.route('/<int:rid>')
 class GenreView(Resource):
+    @auth_required
     def get(self, rid):
         r = genre_service.get_one(rid)
         sm_d = GenreSchema().dump(r)
         return sm_d, 200
+
+    @admin_required
+    def put(self, bid):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = bid
+        genre_service.update(req_json)
+
+        return "", 204
+    @admin_required
+    def delete(self, bid):
+        genre_service.delete(bid)
+        return "", 204
